e6b68e5eb8d6b828db245a008a7d39b6bda26c14
tetsuya.sakayori <sakayori.tetsuya@compass-japan.com>
2026-06-08 15:37:54 +0900

    Add sample test

---
+++ demo/src/test/java/com/example/demo/SampleTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SampleTest {
+
+    @Test
+    void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
